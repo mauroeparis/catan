e59e43a0f6c35bb33fdaca9b59b5e131b212d3f2
Julian Merida <julianmr97@gmail.com>
2019-10-20 10:54:30 -0300

    Add api action call but it still needed to fix some issues

--- src/components/BankTradeComp.jsx
+++ src/components/BankTradeComp.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import _ from "lodash";
 import PropTypes from "prop-types";
+import axios from "axios";
 import CatanTypes from "../CatanTypes";
+
+import API from "../Api";
+
+// TODO
+// agregar arrays al estado
+// agregar estado a todo
+// ver como modificar los recursos, que llamadas hay que hacer
 
 function BankTradeComp({ resources }) {
   const amounts = _.countBy(resources);
   const availableButtons = _.pickBy(amounts, x => x >= 4);
   const refreshButtons = _.keys(availableButtons);
 
-  const currentResource = [];
-  const buttonTable = {
+  // TODO : Should use state to re-render enabled buttons
+  const [state, setState] = useState();
+
+// This is part of Api.js
+  const games = {
+    all: () => API.get("/games/"),
+    get: id => API.get(`/games/${id}`),
+    board: id => API.get(`/games/${id}/board`),
+    player: id => API.get(`/games/${id}/player`),
+    actions: id => API.get(`/games/${id}/player/actions`),
+    makeAction: (id, action, payload) =>
+      API.post(`/games/${id}/player/actions`, { action, payload }),
+    transactions: id => API.get(`/games/${id}/player/transactions`)
+  };
+
+  let giveResource;
+  let receiveResource;
+
+  let buttonTable = {
     brick: true,
     lumber: true,
     wool: true,
@@ -17,80 +42,106 @@ function BankTradeComp({ resources }) {
     ore: true
   };
 
-  function pushResource(value) {
-    currentResource.length = 0;
-    currentResource.push(value);
-    return currentResource;
+  function setGiveResource(res) {
+    giveResource = res;
   }
 
-  function disableButtons(manyResources, table) {
+  function setReceiveResource(res) {
+    receiveResource = res;
+  }
+
+  function EnableClickableButtons(manyResources, table) {
     const newTable = table;
     manyResources.forEach(res => (newTable[res] = false));
     return newTable;
   }
 
-  const newButtonTable = disableButtons(refreshButtons, buttonTable);
+  const newButtonTable = EnableClickableButtons(refreshButtons, buttonTable);
 
-  // Falta deshabilitar los botones que no se puedan usar usando availableButtons
+  function TradeResources(give, receive) {
+    const gameId = 1; // TODO: Should come from an upper state
+    const t = "You are going to exchange 4 of your resource for 1 selected from the bank.";
+    if (window.confirm(t)) {
+      games.makeAction(gameId, "trade_bank", { give, receive });
+      console.log(give);
+      console.log(receive);
+    }
+  }
+
+  function FlushSelectedResources() {
+    giveResource = undefined;
+    receiveResource = undefined;
+    buttonTable = {
+      brick: true,
+      lumber: true,
+      wool: true,
+      grain: true,
+      ore: true
+    };
+  }
+
   return (
     <div>
       <div>
         <button
           type="button"
-          onClick={() => pushResource("brick")}
+          onClick={() => setGiveResource("brick")}
           disabled={newButtonTable.brick}
         >
           Give Brick
         </button>
         <button
           type="button"
-          onClick={() => pushResource("lumber")}
+          onClick={() => setGiveResource("lumber")}
           disabled={newButtonTable.lumber}
         >
           Give Lumber
         </button>
         <button
           type="button"
-          onClick={() => pushResource("wool")}
+          onClick={() => setGiveResource("wool")}
           disabled={newButtonTable.wool}
         >
           Give Wool
         </button>
         <button
           type="button"
-          onClick={() => pushResource("Grain")}
+          onClick={() => setGiveResource("Grain")}
           disabled={newButtonTable.grain}>
           Give Grain
         </button>
         <button
           type="button"
-          onClick={() => pushResource("Ore")}
+          onClick={() => setGiveResource("Ore")}
           disabled={newButtonTable.ore}>
           Give Ore
         </button>
       </div>
       <div>
-        <button type="button" onClick={() => pushResource("brick")}>
-          Get Brick
+        <button type="button" onClick={() => setReceiveResource("brick")}>
+          Receive Brick
         </button>
-        <button type="button" onClick={() => pushResource("lumber")}>
-          Get Lumber
+        <button type="button" onClick={() => setReceiveResource("lumber")}>
+          Receive Lumber
         </button>
-        <button type="button" onClick={() => pushResource("wool")}>
-          Get Wool
+        <button type="button" onClick={() => setReceiveResource("wool")}>
+          Receive Wool
         </button>
-        <button type="button" onClick={() => pushResource("Grain")}>
-          Get Grain
+        <button type="button" onClick={() => setReceiveResource("Grain")}>
+          Receive Grain
         </button>
-        <button type="button" onClick={() => pushResource("Ore")}>
-          Get Ore
+        <button type="button" onClick={() => setReceiveResource("Ore")}>
+          Receive Ore
         </button>
       </div>
       <div>
-        <button type="button" onClick={() => alert("accept")}>
+        <button
+          type="button"
+          onClick={() => TradeResources(giveResource, receiveResource)}
+        >
           Accept
         </button>
-        <button type="button" onClick={() => alert("cancel")}>
+        <button type="button" onClick={FlushSelectedResources}>
           Cancel
         </button>
       </div>
