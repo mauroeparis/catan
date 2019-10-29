feeef429853e99cc82d9f192595a3c5eb09cd43f
Mateo de Mayo <mateodemayo@gmail.com>
2019-10-29 20:39:30 -0300

    Split BankTradeContainer

--- src/components/BankTrade.jsx
+++ src/components/BankTrade.jsx
@@ -7,10 +7,18 @@ import { Link, useParams, useHistory } from "react-router-dom";
 import CatanTypes from "../CatanTypes";
 import api from "../Api";
 
-function BankTrade() {
+export default function BankTrade() {
   const [{ resources }, setResources] = useState({ resources: null });
+  const [{ offer, request }, setTradeState] = useState({
+    offer: null,
+    request: null
+  });
+
   const { gameId } = useParams();
   const history = useHistory();
+
+  // Resources the player can offer for a bank trade (i.e. has more than 4)
+  const canOffer = _.keys(_.pickBy(_.countBy(resources), a => a >= 4));
 
   useEffect(() => {
     const fetchResources = async () => {
@@ -22,83 +30,81 @@ function BankTrade() {
     return () => clearInterval(interval);
   }, [gameId]);
 
-  const amounts = _.countBy(resources);
-  const availableButtons = _.pickBy(amounts, x => x >= 4);
-  const refreshButtons = _.keys(availableButtons);
-
-  const [{ giveResource, receiveResource }, setState] = useState({
-    giveResource: null,
-    receiveResource: null
-  });
-
-  function tradeResources() {
-    const t = `Trading 4 ${giveResource} for 1 ${receiveResource}, are you sure?`;
+  function trade() {
+    // callback that is called from the container, it uses its current scope
+    // for knowing offer and request variables
+    const t = `Trading 4 ${offer} for 1 ${request}, are you sure?`;
     if (window.confirm(t)) {
       api.games.playAction(gameId, "bank_trade", {
-        give: giveResource,
-        receive: receiveResource
+        give: offer,
+        receive: request
       });
       history.push(`/game/${gameId}`);
     }
   }
 
-  const setGiveResource = res =>
-    setState({ giveResource: res, receiveResource });
+  if (!resources) return <i>Determining Trade Options...</i>;
+  return (
+    <BankTradeContainer
+      offer={offer}
+      request={request}
+      canOffer={canOffer}
+      gameId={gameId}
+      setTradeState={setTradeState}
+      trade={trade}
+    />
+  );
+}
 
-  const setReceiveResource = res =>
-    setState({ giveResource, receiveResource: res });
-
+function BankTradeContainer({
+  offer = null,
+  request = null,
+  canOffer,
+  gameId,
+  setTradeState,
+  trade
+}) {
+  const setOffer = resource => setTradeState({ offer: resource, request });
+  const setRequest = resource => setTradeState({ offer, request: resource });
   const RESOURCES = ["brick", "lumber", "wool", "grain", "ore"];
-
   return (
     <div className="bank-trade">
       <h1>Bank Trading</h1>
-      <div className="giving">
+      <div className="offer">
         <span>Give 4</span>
         {RESOURCES.map(resource => (
           <button
             key={resource}
-            className={`${resource} ${
-              giveResource === resource ? "selected" : ""
-            }`}
+            className={`${resource} ${offer === resource ? "selected" : ""}`}
             type="button"
-            onClick={() => setGiveResource(resource)}
-            disabled={!refreshButtons.includes(resource)}
+            onClick={() => setOffer(resource)}
+            disabled={!canOffer.includes(resource)}
           >
-            {resource[0].toUpperCase() + resource.substring(1)}
+            {_.capitalize(resource)}
           </button>
         ))}
       </div>
-      <div className="receiving">
+      <div className="request">
         <span>Recv 1</span>
         {RESOURCES.map(resource => (
           <button
             key={resource}
-            className={`${resource} ${
-              receiveResource === resource ? "selected" : ""
-            }`}
+            className={`${resource} ${request === resource ? "selected" : ""}`}
             type="button"
-            onClick={() => setReceiveResource(resource)}
+            onClick={() => setRequest(resource)}
           >
-            {resource[0].toUpperCase() + resource.substring(1)}
+            {_.capitalize(resource)}
           </button>
         ))}
       </div>
       <div className="footer">
         <Link to={`/game/${gameId}`}>
-          <button
-            type="button"
-            onClick={() =>
-              setState({ giveResource: null, receiveResource: null })
-            }
-          >
-            Cancel
-          </button>
+          <button type="button">Cancel</button>
         </Link>
         <button
           type="button"
-          onClick={() => tradeResources(giveResource, receiveResource)}
-          disabled={giveResource === null || receiveResource === null}
+          onClick={trade}
+          disabled={offer === null || request === null}
         >
           Trade
         </button>
@@ -107,4 +113,20 @@ function BankTrade() {
   );
 }
 
-export default BankTrade;
+BankTradeContainer.propTypes = {
+  offer: CatanTypes.Resource,
+  request: CatanTypes.Resource,
+  canOffer: PropTypes.arrayOf(CatanTypes.Resource).isRequired,
+  gameId: PropTypes.string.isRequired,
+  setTradeState: PropTypes.func.isRequired,
+  trade: PropTypes.func.isRequired
+};
+
+// NOTE: Eventually defaultProps will be deprecated, but for now eslint
+// has not been updated to use es6 default parameters instead
+// https://github.com/yannickcr/eslint-plugin-react/issues/666
+// when it happens, move this and other occurrences as default parameters
+BankTradeContainer.defaultProps = {
+  offer: null, // NOTE: Eventually defaultProps
+  request: null
+};
