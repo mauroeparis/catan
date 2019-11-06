89cb454a752f7f7f76bb2d1bdac8121ba6d7f48d
Mateo de Mayo <mateodemayo@gmail.com>
2019-11-06 17:10:38 -0300

    Add initial modal component

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -8,6 +8,7 @@ import { ResourceList } from "./ResourceList";
 import BuyCard from "./BuyCard";
 import DiceRoll from "./DiceRoll";
 import EndTurn from "./EndTurn";
+import Modal from "./Modal";
 
 function Game() {
   const { id } = useParams();
@@ -24,6 +25,17 @@ function Game() {
         <DiceRoll gameId={id} />
         <EndTurn />
       </div>
+      <Modal
+        disabled
+        title="This is a modal title"
+        body="This is the modal very very long basdaksdjaksdja lsdjalks djalksjd laksjd lkasjd klasjd lkajsd lkajsd lkasjdkl asjd lkasjd lkasjdody"
+        buttons={[
+          { text: "Mateo", callback: () => console.log("mateo") },
+          { text: "Julian", callback: () => console.log("julian") },
+          { text: "Mayco", callback: () => console.log("mayco") },
+          { text: "No One", callback: () => console.log("no one") }
+        ]}
+      />
     </div>
   );
 }
