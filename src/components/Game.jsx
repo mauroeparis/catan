8c84c1a40038e7b3de8f051a65361daa114d8fbd
Mateo de Mayo <mateodemayo@gmail.com>
2019-11-17 15:05:57 -0300

    Make a quick and dirty way to cancel play_knight

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,7 +4,12 @@ import { useParams } from "react-router-dom";
 import { ToastProvider } from "react-toast-notifications";
 import _ from "lodash";
 
-import GameContext, { gameReducer, initGameState } from "../GameContext";
+import GameContext, {
+  gameReducer,
+  initGameState,
+  SET_DEFAULT,
+  PLAY_KNIGHT
+} from "../GameContext";
 import Board from "./Board";
 import CardList from "./CardList";
 import ResourceList from "./ResourceList";
@@ -39,16 +44,26 @@ function Game() {
           <DiceRoll />
           <EndTurn />
         </div>
+        {/* TODO: Find a better way to inform the player the current phase */}
+        <i style={{ position: "fixed", top: "1rem", right: "1rem" }}>
+          {/* TODO: Find a better way to let the player cancel play_knight */}
+          {[PLAY_KNIGHT].includes(game.phase) && (
+            <button
+              className="close"
+              type="button"
+              onClick={() => gameDispatch({ type: SET_DEFAULT })}
+            >
+              ☓
+            </button>
+          )}
+          {_.startCase(game.phase)}
+        </i>
         <Modal
           disabled={disabled}
           title={title}
           body={body}
           buttons={buttons}
         />
-        {/* TODO: Find a better way to inform the player the current phase */}
-        <i style={{ position: "fixed", top: "1rem", right: "1rem" }}>
-          {_.startCase(game.phase)}
-        </i>
       </div>
     </GameContext.Provider>
   );
