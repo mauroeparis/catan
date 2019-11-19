e218d9ef6a3da2121139d6c1d3fc7514ed881b28
Mauro Ezequiel Paris <mauroparis96@gmail.com>
2019-11-19 10:35:52 -0300

    add whose turn it is in gamestatus

--- src/components/GameStatus.jsx
+++ src/components/GameStatus.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import _ from "lodash";
 
 import GameContext, {
@@ -7,11 +7,41 @@ import GameContext, {
   PLAY_ROAD_BUILDING
 } from "../GameContext";
 import api from "../Api";
+import { ReactComponent as Dot } from "../public/icons/circle-solid.svg";
 
 export default function GameStatus() {
   const { phase, gameId, roads, gameDispatch, showModal } = useContext(
     GameContext
   );
+
+  const [{ username, colour }, setTurn] = useState({
+    username: null,
+    colour: "yellow"
+  });
+
+  useEffect(() => {
+    const fetchTurn = async () => {
+      const res = await api.games.get(gameId);
+      const usernameTurn = res.data.current_turn.user;
+      const colourTurn = res.data.players.find(p => p.username === usernameTurn)
+        .colour;
+
+      setTurn({
+        username: usernameTurn,
+        colour: colourTurn
+      });
+    };
+    fetchTurn();
+    const interval = setInterval(() => fetchTurn(), api.POLL_EVERY);
+    return () => clearInterval(interval);
+  }, [gameId]);
+
+  const playerColours = {
+    red: "#F44336",
+    blue: "#2196F3",
+    yellow: "#FFC107",
+    white: "#4CAF50"
+  };
 
   const statusClasses =
     "bg-gray-400 p-3 text-gray-800 rounded-lg w-40 h-auto text-center";
@@ -25,40 +55,54 @@ export default function GameStatus() {
   return (
     <div>
       {/* TODO: Find a better way to inform the player the current phase */}
-      <div
-        className={statusClasses}
-        style={{ position: "fixed", top: "1rem", right: "1rem" }}
-      >
-        {_.startCase(phase)}
-        {/* TODO: Find a better way to let the player cancel play_knight */}
-        {[PLAY_KNIGHT, PLAY_ROAD_BUILDING].includes(phase) && (
-          <button
-            className={cancelBtnClasses}
-            type="button"
-            onClick={() => gameDispatch({ type: SET_DEFAULT })}
+      <div style={{ position: "fixed", top: "1rem", right: "1rem" }}>
+        <div className="pb-3 flex flex-row">
+          <div
+            className="self-center w-4 h-4 mr-5 inline-block"
+            style={{ color: playerColours[colour] }}
           >
-            cancel
-          </button>
-        )}
-        {[PLAY_ROAD_BUILDING].includes(phase) && (
-          <button
-            className={successBtnClasses}
-            type="button"
-            onClick={() => {
-              if (roads.length === 2) {
-                api.games.playAction(gameId, "play_road_building_card", roads);
-                gameDispatch({ type: SET_DEFAULT });
-              } else
-                showModal({
-                  disabled: false,
-                  title: "Select two roads first",
-                  buttons: [{ text: "Ok" }]
-                });
-            }}
-          >
-            done
-          </button>
-        )}
+            <Dot />
+          </div>
+          <span>
+            {username ? `${_.capitalize(username)}'s Turn` : "Loading Turn..."}
+          </span>
+        </div>
+        <div className={statusClasses}>
+          {_.startCase(phase)}
+          {/* TODO: Find a better way to let the player cancel play_knight */}
+          {[PLAY_KNIGHT, PLAY_ROAD_BUILDING].includes(phase) && (
+            <button
+              className={cancelBtnClasses}
+              type="button"
+              onClick={() => gameDispatch({ type: SET_DEFAULT })}
+            >
+              cancel
+            </button>
+          )}
+          {[PLAY_ROAD_BUILDING].includes(phase) && (
+            <button
+              className={successBtnClasses}
+              type="button"
+              onClick={() => {
+                if (roads.length === 2) {
+                  api.games.playAction(
+                    gameId,
+                    "play_road_building_card",
+                    roads
+                  );
+                  gameDispatch({ type: SET_DEFAULT });
+                } else
+                  showModal({
+                    disabled: false,
+                    title: "Select two roads first",
+                    buttons: [{ text: "Ok" }]
+                  });
+              }}
+            >
+              done
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
